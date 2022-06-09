2d2a99d97712983e00af8cd0ce91ccfbef90bf96
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-06-09 12:32:13 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(function () {
+    const swiper = new Swiper('.index', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                navigation: false
+            },
+
+            768: {
+                slidesPerView: 2
+            },
+
+            1024: {
+                slidesPerView: 3
+            }
+        }
+    });
+
+    const swiper2 = new Swiper('.tour-img', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    $('.menu-btn').on('click', function () {
+        $('.menu').slideDown();
+        $('body').addClass('active');
+    });
+
+    $('.menu-exit').on('click', function () {
+        $('.menu').slideUp();
+        $('body').removeClass('active');
+    });
+
+    $('.c-list').click(function () {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.itemBox').show(500);
+        }
+        else {
+            $('.itemBox').not('.'+value).hide(500);
+            $('.itemBox').filter('.'+value).show(500);
+        }
+    });
+    $('.c-list').click(function () {
+       $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    $('.c-photo').lightGallery();
+
+
+})
